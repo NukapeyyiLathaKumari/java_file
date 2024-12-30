bb48b84ac1250418e1171ac0305545d84221fade
Latha <322103282071.kumari@gvpcew.ac.in>
2024-12-30 14:59:58 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,6 @@
+public static void main(String args[])
+  {
+    System.out.println("The value of a:10");
+    System.out.println("The value of b:20");
+    System.out.println("The addition of a and b is:",a+b);
+}
